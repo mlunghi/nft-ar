3891097e623f0f18b19bfd5958326112140ea286
mlunghi <matteo.lunghi2000@gmail.com>
2021-02-04 13:32:39 +0100

    reducing size

--- places.js
+++ places.js
@@ -95,7 +95,7 @@ function renderPlaces(places) {
         icon.setAttribute('src', '../assets/map-marker.png');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale', '20, 20');
+        icon.setAttribute('scale', '10, 10');
 
         icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
