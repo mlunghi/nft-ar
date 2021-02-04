bcb7ebad4b6c70f27f13e27794eb53efce0165d9
mlunghi <matteo.lunghi2000@gmail.com>
2021-02-04 14:09:10 +0100

    right coordinates

--- places.js
+++ places.js
@@ -35,8 +35,8 @@ function staticLoadPlaces() {
         {
             name: "Your place name",
             location: {
-                lat: 45.0971209, // add here latitude if using static data
-                lng: 8.7436960, // add here longitude if using static data
+                lat: 45.0971509, // add here latitude if using static data
+                lng: 8.7436701, // add here longitude if using static data
             }
         },
         {
