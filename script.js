cec0cde57ef47933e33f15040d566fcbaee40d02
mlunghi <matteo.lunghi2000@gmail.com>
2021-02-22 12:57:10 +0100

    animate test

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ var models = [
         info: 'Ataberk Friend2 Stature',
     },
     {
-        url: './assets/ata/pink.gltf',
+        url: './assets/ata/animated.gltf',
         scale: '1 1 1',
         rotation: '0 180 0',
         info: 'Horns Stature',
